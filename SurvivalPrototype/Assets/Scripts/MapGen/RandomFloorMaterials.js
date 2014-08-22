1c1afffda3c76b03d286727e59524ba8c2c1100b
Jacob Gravelle <jacob@pocketgems.com>
2014-08-21 19:43:28 -0700

    Sexify the ground materials, select them randomly

---
+++ SurvivalPrototype/Assets/Scripts/MapGen/RandomFloorMaterials.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+public var materialPossibilities : Material[];
+
+function Start() {
+	for (var i = 0; i < transform.childCount; i++) {
+		var obj = transform.GetChild(i);
+		obj.renderer.material = materialPossibilities[Random.Range(0, materialPossibilities.length)];
+	}
+}
