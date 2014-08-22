b449be43aff2bd8630320ed18da460dbcac096b9
Jacob Gravelle <jacob@pocketgems.com>
2014-08-22 11:16:53 -0700

    Checkpoint icon spawning

--- SurvivalPrototype/Assets/Scripts/IconSpawner.cs
+++ SurvivalPrototype/Assets/Scripts/IconSpawner.cs
@@ -3,9 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class IconSpawner : MonoBehaviour {
+	public GameObject iconPrefab;
+
+	private GameObject uiCamera;
+	private GameObject player;
+
+	private GameObject spawnedIcon;
+
 	void Start() {
+		uiCamera = GameObject.Find("2dCamera");
+		player = GameObject.Find("Player");
 	}
 	
 	void Update() {
+		if (spawnedIcon == null) {
+			spawnedIcon = GameObject.Instantiate(iconPrefab) as GameObject;
+		}
+
+		if (spawnedIcon != null) {
+			Camera c = uiCamera.camera;
+			// spawnedIcon.transform.position = 
+		}
 	}
 }
